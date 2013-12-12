a69b281ad870f7425bc700bf1154ba33a58c888f
William Lebel <lebelwilliam@gmail.com>
2013-12-12 15:38:25 -0500

    Remove all the MySQL stuff. Not needed.

--- src/com/jumanjicraft/BungeeChatClient/BungeeListener.java
+++ src/com/jumanjicraft/BungeeChatClient/BungeeListener.java
@@ -1,13 +1,11 @@
 package com.jumanjicraft.BungeeChatClient;
 
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import com.dthielke.herochat.ChannelChatEvent;
 import com.dthielke.herochat.Chatter;
 import com.dthielke.herochat.Herochat;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 
 public class BungeeListener implements Listener {
 	
@@ -29,34 +27,4 @@ public class BungeeListener implements Listener {
 			}
 		}
 	}
-	
-	@EventHandler(priority=EventPriority.MONITOR)
-	public void onPlayerJoin(PlayerJoinEvent event)
-	{
-		String nick = plugin.getNickName(event.getPlayer().getName());
-		if (nick != null)
-		{
-			event.getPlayer().setDisplayName("*"+nick);
-		}
-		/*int mins = plugin.getMuteMinutes(event.getPlayer());
-		if (mins > 0)
-		{
-			plugin.newMuteTask(event.getPlayer());
-			plugin.hcMute(event.getPlayer(), true);
-		} else if (mins == 0) {
-			plugin.hcMute(event.getPlayer(), true);
-		} else {
-			plugin.hcMute(event.getPlayer(), false);
-		}*/
-	}
-	
-	/*@EventHandler(priority=EventPriority.MONITOR)
-	public void onPlayerQuit(PlayerQuitEvent event)
-	{
-		if (plugin.tempmutes.containsKey(event.getPlayer().getName()))
-		{
-			Bukkit.getScheduler().cancelTask(plugin.tempmutes.get(event.getPlayer().getName()));
-			plugin.tempmutes.remove(event.getPlayer().getName());
-		}
-	}*/
 }
